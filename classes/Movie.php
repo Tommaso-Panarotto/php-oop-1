088814113a95d43dd543123ab0ad3b4dbce7e34c
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-09 15:52:40 +0200

    constructor

--- classes/Movie.php
+++ classes/Movie.php
@@ -8,4 +8,13 @@ class Movie
     public $genre;
     public $description;
     public $classification;
+
+    //constructor
+    function __construct($_title, $_description, $_genre, $_classification)
+    {
+        $this->title = $_title;
+        $this->description = $_description;
+        $this->genre = $_genre;
+        $this->classification = $_classification;
+    }
 }
