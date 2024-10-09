076d8e10b3d3f5f1ed99bd2582a0f8956df5bb9b
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-09 17:10:33 +0200

    added bootstrap

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require __DIR__ . "/db.php";
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 
@@ -5,10 +9,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP OOP</title>
+    <!--Boostrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>PHP OOP</h1>
+
 </body>
 
 </html>
