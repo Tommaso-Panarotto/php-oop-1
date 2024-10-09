51067a25164c214c34a4eff1c1d7cf8333abee4d
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-09 17:07:57 +0200

    added comments

--- db.php
+++ db.php
@@ -2,13 +2,19 @@
 
 require __DIR__ . "/classes/Movie.php";
 
+//Movie array
+$movies = [];
+
+//Instance a new movie
 $deadpool = new Movie('Deadpool', 'Wade Wilson è un ex membro delle Forze Speciali diventato mercenario. Dopo un esperimento non autorizzato che gli conferisce il potere di guarigione veloce.', 'Azione', '18+');
+//save the new movie in the array
+$movies[] = $deadpool;
+
+//Instance a new movie
 $notteAlMuseo = new Movie(
     'Una notte al museo',
     'A New York, Larry Daley è un padre divorziato che vive di precariato. La situazione poco stabile dell\'uomo non giova ai già difficili rapporti col figlio Nick, che ha dieci anni e vive con la madre.',
-    'Commedia',
-    ''
+    'Commedia'
 );
-
-var_dump($deadpool);
-var_dump($notteAlMuseo);
+//save the new movie in the array
+$movies[] = $notteAlMuseo;
